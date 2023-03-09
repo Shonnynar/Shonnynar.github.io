13a9b8b376eee475111ca0ee94c964388f2e84ea
Shonnynar <127359333+Shonnynar@users.noreply.github.com>
2023-03-08 21:12:53 -0300

    Initial commit

---
+++ function.js
@@ -0,0 +1,6 @@
+
+
+document.getElementById("boton").onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML = "Bienvenidos a mi presentación";
+}
